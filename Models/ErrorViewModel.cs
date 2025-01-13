e7930eb3e3cf8a3af7926f5c69888508de9e33af
guthriegobar <pmadhikari@gmail.com>
2025-01-13 15:15:58 +1100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CareHomeInfoTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
